bcb9ce184cfa6b42861305adcf95d7bc782f31a8
Santiago Villarreal <santivilla795@hotmail.com>
2024-08-27 22:22:49 -0300

    Python clase 2

---
+++ Santiago-Villarreal/LaboratorioIV/Python/4semestre/clase01/pythonProject/Sistemas_numeracion.py
@@ -0,0 +1,36 @@
+
+# Sistema decimal
+a = 10
+print(f'a decimal: {a}')
+
+# sistema binario
+a = 0b1010
+print(f'a binario: {a}')
+
+# Sistema octal
+a = 0o12
+print(f'a octal: {a}')
+
+# Sistema hexadecimal
+a = 0xA
+print(f'a hexadecimal: {a}')
+
+# base decimal
+a = int('23', 10)
+print(f'a base decimal: {a}')
+
+# base binario
+a = int('10111', 2)
+print(f'a base binaria: {a}')
+
+# base octal
+a = int('27', 8)
+print(f'a base octal: {a}')
+
+# base hexadecimal
+a = int('17', 16)
+print(f'a base hexadecimal: {a}')
+
+# base 5
+a = int('34', 5)
+print(f'a base 5: {a}')
