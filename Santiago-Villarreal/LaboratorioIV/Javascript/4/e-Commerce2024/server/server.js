b28cb92e865bd8b86dc127c59988397f7f3ab805
Santiago Villarreal <santivilla795@hotmail.com>
2024-09-16 22:36:10 -0300

    javascript ecommerce terminado

--- Santiago-Villarreal/LaboratorioIV/Javascript/4/e-Commerce2024/server/server.js
+++ Santiago-Villarreal/LaboratorioIV/Javascript/4/e-Commerce2024/server/server.js
@@ -1,59 +1,53 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+
+import { MercadoPagoConfig, Preference } from "mercadopago";
+const client = new MercadoPagoConfig({
+  accessToken: "TEST-7335359473520890-091621-54f835d715c315d7da3114a3517bd70c-192267643", 
+});
+
 const app = express();
-const cors = require("cors");
-const mercadopago = require("mercadopago");
-const path = require("path");
+const port = 3000;
 
-// REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel
-mercadopago.configure({
-	access_token: "<ACCESS_TOKEN>",
-});
-
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(express.static(path.join(__dirname,"../client")));
 app.use(cors());
-app.get("/", function () {
-	path.resolve(__dirname,"..","client","index.html");
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.send("Soy el server");
 });
 
-app.post("/create_preference", (req, res) => {
+app.post("/create_preference", async (req, res) => {
+  try {
+    const body = {
+      items: [
+        {
+          title: req.body.description,
+          unit_price: Number(req.body.price),
+          quantity: Number(req.body.quantity),
+          currency_id: "ARS",
+        },
+      ],
+      back_urls: {
+        success: "http://127.0.0.1:5500/client/media/index.html",
+        failure: "http://127.0.0.1:5500/client/media/index.html",
+        pending: "http://127.0.0.1:5500/client/media/index.html",
+      },
+      auto_return: "approved",
+    };
 
-	let preference = {
-		items: [
-			{
-				title: req.body.description,
-				unit_price: Number(req.body.price),
-				quantity: Number(req.body.quantity),
-			}
-		],
-		back_urls: {
-			"success": "http://localhost:8080/",
-			"failure": "http://localhost:8080/",
-			"pending": "",
-		},
-		auto_return: "approved",
-	};
-
-	mercadopago.preferences.create(preference)
-		.then(function (response) {
-			res.json({
-				id: response.body.id
-			});
-		}).catch(function (error) {
-			console.log(error);
-		});
+    const preference = new Preference(client);
+    const result = await preference.create({ body });
+    res.json({
+      id: result.id,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: "Error al crear la preferencia",
+    });
+  }
 });
 
-app.get('/feedback', function (req, res) {
-	res.json({
-		Payment: req.query.payment_id,
-		Status: req.query.status,
-		MerchantOrder: req.query.merchant_order_id
-	});
-});
-
-app.listen(8080, () => {
-	console.log("The server is now running on Port 8080");
+app.listen(port, () => {
+	console.log(`El servidor esta corriendo en el puerto ${port}`);
 });
