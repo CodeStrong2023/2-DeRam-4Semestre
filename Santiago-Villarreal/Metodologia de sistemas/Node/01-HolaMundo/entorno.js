41aac77600c185ba19310e1bb74a1f0e305a6be6
Santiago Villarreal <santivilla795@hotmail.com>
2024-09-26 21:37:30 -0300

    Node clase 7

--- Santiago-Villarreal/Metodologia de sistemas/Node/01-HolaMundo/entorno.js
+++ Santiago-Villarreal/Metodologia de sistemas/Node/01-HolaMundo/entorno.js
@@ -2,3 +2,5 @@ let nombre = process.env.NOMBRE || 'Sin nombre';
 let web = process.env.WEB ||  'No tengo web';
 console.log('Hola ' + nombre);
 console.log('Mi web es: ' + web);
+
+console.log('Ultima ejecucion');
