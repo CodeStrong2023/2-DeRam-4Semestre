e83d395c5b4288fd890d0fa75d18833ef8fe3544
Santiago Villarreal <santivilla795@hotmail.com>
2024-10-03 23:03:42 -0300

    java clase 8

--- Santiago-Villarreal/Metodologia de sistemas/Java/4s/clase06/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
+++ Santiago-Villarreal/Metodologia de sistemas/Java/4s/clase06/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -1,13 +1,26 @@
 package utn.tienda_libros;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import utn.tienda_libros.vista.LibroFrom;
 
 @SpringBootApplication
 public class TiendaLibrosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TiendaLibrosApplication.class, args);
+		ConfigurableApplicationContext contextoSpring =
+				new SpringApplicationBuilder(TiendaLibrosApplication.class)
+						.headless(false)
+						.web(WebApplicationType.NONE)
+						.run(args);
+
+		// 	Ejecutar codigo para cargar formulario
+		java.awt.EventQueue.invokeLater(() -> {
+			LibroFrom libroFrom = contextoSpring.getBean(LibroFrom.class);
+			libroFrom.setVisible(true);
+		});
 	}
 
 }
