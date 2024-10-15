73c3ba57b46c40a1a831eaacd579639715712248
Santiago Villarreal <santivilla795@hotmail.com>
2024-10-14 21:40:35 -0300

    python clase 5 string

---
+++ Santiago-Villarreal/LaboratorioIV/Python/4semestre/clase01/pythonProject/metodo_split.py
@@ -0,0 +1,12 @@
+# help(str.split)
+
+cursos = 'Java JavaScript Node Python Diseno'
+lista_cursos = cursos.split()
+print(f'Lista de cursos: {lista_cursos}')
+print(type(lista_cursos))
+
+cursos_separados_coma = 'Java,Python,Node,Javascript,Spring'
+lista_cursos = cursos_separados_coma.split(',')
+print(f'Lista de cursos: {lista_cursos}')
+print(len(lista_cursos))
+
---
+++ Santiago-Villarreal/LaboratorioIV/Python/4semestre/clase01/pythonProject/mi_clase.py
@@ -0,0 +1,18 @@
+class miClase:
+    '''
+    Ejemplo de documentación de clase
+    '''
+    def __init__(self):
+        '''
+        Metodo dunder init
+        Metodo de inicio de la clase
+        '''
+
+    def mi_metodo(self, param1, param2):
+        '''
+        Metodo con parametros
+        Detecta los parametros automaticamente
+        :param param1: parametro 1
+        :param param2: parametro 2
+        :return: valor de retorno
+        '''
