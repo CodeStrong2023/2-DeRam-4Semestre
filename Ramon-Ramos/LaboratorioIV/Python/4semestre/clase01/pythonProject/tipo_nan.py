48632cd4b18dd96afe112f84e83004120a7f4d6a
RamonRamos1 <108014511+RamonRamos1@users.noreply.github.com>
2024-10-15 18:18:34 -0300

    Laboratorio 4

---
+++ Ramon-Ramos/LaboratorioIV/Python/4semestre/clase01/pythonProject/tipo_nan.py
@@ -0,0 +1,5 @@
+import math
+
+a = float('NaN')
+print(f'a={a}')
+print(f'a es nan? = {math.isnan(a)}')
---
+++ Ramon-Ramos/LaboratorioIV/Python/4semestre/clase01/pythonProject/tipo_str.py
@@ -0,0 +1,10 @@
+import math
+
+# concatenacion automatica
+
+variable = 'adios'
+mensaje = 'hola ' + 'alumno' + variable
+mensaje += ', terminado'
+print(mensaje)
+# clase help
+help(math.isnan)
