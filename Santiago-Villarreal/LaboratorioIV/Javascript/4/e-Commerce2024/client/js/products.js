33d5d7332a225e4f8f31ad9427ab9b638c72f52c
Santiago Villarreal <santivilla795@hotmail.com>
2024-10-16 19:49:20 -0300

    Reapply "Clase 1 javascript ecommerce"
    This reverts commit b6e3381077b91f49773733fdd26f609d859446cb.

---
+++ Santiago-Villarreal/LaboratorioIV/Javascript/4/e-Commerce2024/client/js/products.js
@@ -0,0 +1,44 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Microfono Condenser",
+        price: 500,
+        quanty: 1,
+        img: "/client/media/mcondenser.jpg",
+    },
+    {
+        id:2,
+        productName: "Pie",
+        price:150,
+        quanty: 1,
+        img: "/client/media/pie.jpg",
+    },
+    {
+        id: 3,
+        productName: "USB",
+        price:500,
+        quanty: 1,
+        img: "/client/media/usbcondenser.jpg",
+    },
+    {
+        id: 4,
+        productName: "Condenser Samson",
+        price: 500,
+        quanty: 1,
+        img: "/client/media/samsoncondenser.jpg",
+    },
+    {
+        id: 5,
+        productName: "Manual Condenser",
+        price: 100,
+        quanty: 1,
+        img: "/client/media/samsonmanual.jpg",
+    },
+    {
+        id: 6,
+        productName: "c01upro",
+        price: 500,
+        quanty: 1,
+        img: "/client/media/c01upro.jpg",
+    },
+];
