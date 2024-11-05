0062486701d11ca41c61618770fdef3a77d78a7e
Lorena Artieda <76899464+ArtiedaLorena@users.noreply.github.com>
2024-11-05 16:16:01 -0300

    Se sube juego Star Wars

---
+++ Artieda-Lorena/Python/Juego_Tuc/pythonProject1/constantes.py
@@ -0,0 +1,17 @@
+import os
+
+# Dimensiones de la pantalla
+SCREEN_WIDTH = 1700
+SCREEN_HEIGHT = 1000
+
+# Colores
+COLOR_LASER = (0, 0, 255)  # Azul
+
+# Ruta a los assets
+ASSETS_PATH = os.path.join(os.path.dirname(__file__), 'assets')
+
+#Ruta de los archivos
+IMPERIAL_MARCH_PATH = f'{ASSETS_PATH}/sounds/Imperial March - kenobi.mp3'
+START_IMAGE_PATH= f'{ASSETS_PATH}/images/inicio/star.png'
+ESTRELLA_PATH= f'{ASSETS_PATH}/images/inicio/estrella'
+FONDO1_PATH = f'{ASSETS_PATH}/images/fondo1.jpg'
--- Artieda-Lorena/Python/PruebaPygame/pythonProject/main.py
+++ Artieda-Lorena/Python/PruebaPygame/pythonProject/main.py
@@ -1,2 +1,21 @@
 #Ejemplo de pantalla en pygame
 import pygame
+
+#Configuracion de pantalla de pygame
+pygame.init()
+screen = pygame.display.set_mode((800,600))
+clock= pygame.time.Clock()
+running= True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+screen.file('purple')
+
+pygame.display.flip()
+
+clock.tick(60)
+
+pygame.quit()
